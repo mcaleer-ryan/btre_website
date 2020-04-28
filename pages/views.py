3a6abc63c7891c59658cbaf1137d1666c200dad6
ryan mcaleer <mcaleer_ryan@bentley.edu>
2020-04-28 18:55:09 -0400

    Bolded Hello World

--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    return HttpResponse('<h1><b>Hello World</b></h1>')
